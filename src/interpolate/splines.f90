51b85da57df5b8c1249e85af2d704cf59b012046
Juan Fiol <juanfiol@gmail.com>
2019-08-07 21:27:00 -0300

    few cosmetic changes

--- src/interpolate/splines.f90
+++ src/interpolate/splines.f90
@@ -144,7 +144,7 @@ contains
       IF (any(A(:n1) < Small)) call print_msg('Points not in strict ascending order',&
         & sub='spline_coeff', errcode=1)
 
-      D(:n1) = (Y(2:) - Y(:nn - 1)) / A(:n1)
+      D(:n1) = (Y(2:) - Y(:n1)) / A(:n1)
 
       do i = 1, n2   ! Symmetric Coefficient Matrix (augmented).
         B(i) = 2._dp * (A(i) + A(i + 1))
@@ -154,12 +154,12 @@ contains
       D(2) = D(2) - A(1) * s1
       D(n1) = D(n1) - A(n1) * sN
 
-      do i = 2, n2   ! Gauss solution of the tridiagonal SYSTEM.
+      do i = 2, n2   ! Gauss solution for the tridiagonal SYSTEM.
         R = A(i) / B(i - 1)
         B(i) = B(i) - R * A(i)
         D(i + 1) = D(i + 1) - R * D(i)
       enddo
-      D(n1) = D(n1) / B(n2)  ! The Sigma Coefficients are stored in array D.
+      D(n1) = D(n1) / B(n2)  ! Sigma Coefficients
 
       do i = 2, n2
         k = nn - i
